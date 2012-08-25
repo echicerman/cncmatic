6b04fa2eab2b5e78a54e84c3c05e85add47a8a5b
nicommorales <nicommorales@8f540ebd-3f1b-4837-e010-a1dbbd819cf1>
2012-08-25 17:15:55 +0000

    Incorporacion del modulo Previsualizador

--- trunk/CNCMatic/Previsualizador/Properties/Settings.Designer.cs
+++ trunk/CNCMatic/Previsualizador/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CNCMatic.Properties {
+namespace Previsualizador.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

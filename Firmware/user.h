a119d5ddf098c3b7368e3c3e5944320c86fac5f6
gabrielg.5 <gabrielg.5@hotmail.com>
2012-10-08 22:48:56 +0000

    * agregado movimientos libres

--- Firmware/user.h
+++ Firmware/user.h
@@ -2,16 +2,15 @@
 #define USER_H
 
 typedef enum {
-	SERIALPORTCONNECTED,		// 0
+	SERIALPORTCONNECTED,	// 0
 	HANDSHAKEACKRECEIVED,	// 1
-	CNCMATICCONNECTED,			// 2
-	CONFIGURED,							// 3
-	WAITINGCOMMAND,					// 4
-	PROCESSINGCOMMAND,			// 5
-	LIMITSENSOR,							// 6
-	EMERGENCYSTOP,					// 7
-	TEST,										// 8
-	ANSWERTEST							// 9
+	CNCMATICCONNECTED,		// 2
+	CONFIGURED,				// 3
+	WAITINGCOMMAND,			// 4
+	PROCESSINGCOMMAND,		// 5
+	LIMITSENSOR,			// 6
+	EMERGENCYSTOP,			// 7
+	FREEMOVES				// 8
 } state_t;
 
 typedef struct 
@@ -28,5 +27,8 @@ typedef struct
 
 void LimitSensorHandler(void);
 void user(void);
+
+position_t CreatePosition(unsigned long, unsigned long, unsigned long);
+position_t CreatePositionFrom(position_t);
 
 #endif

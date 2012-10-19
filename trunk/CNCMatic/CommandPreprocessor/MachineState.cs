21757ac15395df091ebf5f0103225b1bc6fdca77
gabrielg.5@hotmail.com <gabrielg.5@hotmail.com@8f540ebd-3f1b-4837-e010-a1dbbd819cf1>
2012-10-19 02:39:31 +0000

    * CommandPreprocessor project

---
+++ trunk/CNCMatic/CommandPreprocessor/MachineState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandPreprocessor
+{
+    enum MachineState
+    {
+        SERIALPORTCONNECTED,	// 0
+        HANDSHAKEACKRECEIVED,	// 1
+        CNCMATICCONNECTED,		// 2
+        CONFIGURED,				// 3
+        WAITINGCOMMAND,			// 4
+        PROCESSINGCOMMAND,		// 5
+        LIMITSENSOR,			// 6
+        EMERGENCYSTOP,			// 7
+        FREEMOVES				// 8
+    }
+}
---
+++ trunk/CNCMatic/CommandPreprocessor/Position.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandPreprocessor
+{
+    public class Position
+    {
+        #region Properties
+        private double x;
+        private double y;
+        private double z;
+        #endregion
+
+        #region Getters & Setters
+        public double X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+        public double Z
+        {
+            get { return this.z; }
+            set { this.z = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Position(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        public Position()
+        {
+            this.X = this.Y = this.Z = 0;
+        }
+        #endregion
+    }
+}

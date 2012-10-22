efa0f07a639e602bdfbd040181657e701076902a
gabrielg.5 <gabrielg.5@hotmail.com>
2012-10-22 19:20:40 +0000

    * Agregado aplicacion de test de Preprocesador * sacado .pdb de outputs * el firmware devuelve la posicion en la que quedo la punta despues del movimiento

---
+++ CNCMatic/CommandPreprocessorTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandPreprocessor
+{
+    class CommandPreprocessorTest
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** BIENVENIDO AL TEST DEL PREPROCESADOR DE COMANDOS DE CNCmatic ® *****");
+            Console.WriteLine("***** Escriba un comando de codigo G, y el preprocesador le devolverá los comandos procesados que serán enviados a la maquina (Para salir inserte 'exit') *****");
+            Console.WriteLine();
+            
+            string input = Console.ReadLine();
+            CommandPreprocessor.GetInstance();
+            while (input != "exit")
+            {
+                Console.WriteLine("Comandos a enviar a la maquina:");
+                List<string> result = CommandPreprocessor.GetInstance().ProcessCommand(input);
+                for(int i = 0; i < result.Count; i++)
+                {
+                    Console.WriteLine("Comando " + i.ToString("00") + ": " + result[i]);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Escriba nuevo comando o 'exit' para salir");
+                input = Console.ReadLine();
+            }
+            
+        }
+    }
+}
